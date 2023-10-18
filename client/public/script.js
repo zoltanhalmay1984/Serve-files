27d0c5a5ab9ae05037c2acdad9a0a24b368cb977
Zoltán Halmay <132577193+zoltanhalmay1984@users.noreply.github.com>
2023-10-18 16:01:05 +0200

    PATCH part is done

--- client/public/script.js
+++ client/public/script.js
@@ -9,53 +9,72 @@ const buttonHTML = (text, method) => `<button type="submit" data-method="${metho
 const formHTML = (user) => `
 <form id="form" data-id="${user.id}">
   ${inputHTML(user.name)}
-  ${buttonHTML("Save", "")}
+  ${buttonHTML("Save", "PATCH")}
 </form>
 `;
 
-const fetchData = async (url, id) => {
+const fetchData = async (url, id, method = "GET", body = { name: "" }) => {
     try {
-      const response = await fetch(id !== undefined ? `${url}/${id}` : url);
-      return await response.json();
-  
+        const response = await fetch(id !== undefined ? `${url}/${id}` : url, method === "GET" ? { method } : { method, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(body) });
+        return await response.json();
+
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  }
-  
+}
+
+const handleInput = ({ target }) => {
+    target.setAttribute("value", target.value);
+}
+
+const handleSubmit = async e => {
+    e.preventDefault();
+
+    const method = e.submitter.getAttribute("data-method");
+    const id = parseInt(e.target.getAttribute("data-id"));
+    const result = await fetchData(url, id, method, method === "PATCH" ? { name: e.target.querySelector("input").value } : { name: "" });
+    if (result.state === 'DONE') {
+        const users = await fetchData(url);
+        document.getElementById("users").outerHTML = usersHTML(users);
+    }
+    // fetchData(url, e.target.getAttribute("data-id"), method, method === "PATCH" ? { name: e.target.querySelector("input").value } : { name: "" })
+}
 
 const main = async _ => {
     const users = await fetchData(url);
     const root = document.getElementById("root");
     root.insertAdjacentHTML("beforeend", usersHTML(users));
     root.insertAdjacentHTML("beforeend", formHTML({ id: 0, name: "" })); // Insert the form initially
+    window.addEventListener("click", handleClick);
+    window.addEventListener("input", handleInput);
+    window.addEventListener("submit", handleSubmit);
 };
 
 const handleClick = async ({ target }) => {
     const userTarget = target.classList.contains('user') ? target : target.closest('.user');
-  
+
     if (userTarget) {
-      const userId = userTarget.querySelector('.user-id').textContent;
-  
-      const response = await fetch(`${url}/${userId}`);
-      const data = await response.json();
-      
-      // Access the input element within the form
-      const form = document.getElementById('form');
-      const inputElement = form.querySelector('input');
-  
-      // Set the value of the input element
-      inputElement.value = data.name;
-  
-      // Update the data-id attribute of the form
-      form.dataset.id = userId;
-    //   const inputElement = userTarget.querySelector('input');
-    //   inputElement.value = data.name;
-  
-    //   document.getElementById('form').dataset.id = userId;
+        const userId = userTarget.querySelector('.user-id').textContent;
+
+        const response = await fetch(`${url}/${userId}`);
+        const data = await response.json();
+
+        // Access the input element within the form
+        const form = document.getElementById('form');
+        const inputElement = form.querySelector('input');
+
+        // Set the value of the input element
+        inputElement.value = data.name;
+
+        // Update the data-id attribute of the form
+        form.dataset.id = userId;
+        //   const inputElement = userTarget.querySelector('input');
+        //   inputElement.value = data.name;
+
+        //   document.getElementById('form').dataset.id = userId;
     }
-  };
-  
+};
+
 
 window.addEventListener("load", main);
-window.addEventListener("click", handleClick);
+// window.addEventListener("click", handleClick); //korábbi hely, ahol lefutott, most a mainben van
--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import express from "express";
 import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from 'url';
+const dataRoute = './users.json';
 
 const __filename = fileURLToPath(import.meta.url); //ez csak ahhoz kell, hogy létre tudjam hozni a __dirname -t
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/pub', express.static(path.join(__dirname, 'client', 'public')));
 
 app.get('/', (req, res) => {
@@ -31,6 +34,20 @@ app.get('/users/:userId', async (req, res) => {
     const user = users.find(user => user.id === userId);
     if (user) {
         return res.send(user);
+    } else {
+        return res.status(404).send({ state: 'User not found' });
+    }
+});
+
+app.patch('/users/:userId', async (req, res) => {
+    const data = await fs.readFile(dataRoute, 'utf8');
+    const { users /*, age, id */ } = JSON.parse(data); // a JSON.parse(data) users key value-ját menti ki users változóba (data destructuring)
+    const userId = parseInt(req.params.userId);
+    const user = users.find(user => user.id === userId);
+    if (user) {
+        user.name = req.body.name;
+        await fs.writeFile(dataRoute, JSON.stringify({ users }), 'utf8');
+        return res.send({ state: "DONE" });
     } else {
         return res.status(404).send({ state: 'User not found' });
     }
